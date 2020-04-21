3f0452ab15f2bda657b4218b7d0e37b050d8bb17
xuweifeng <shangguanqingyun@baofu.com>
2020-04-21 19:15:00 +0800

    eee

--- server/common.js
+++ server/common.js
@@ -2,7 +2,7 @@
 export default {
   web_name: '阳光内容管理系统',
   // web_domain: 'http://103.27.4.146:3001', // 访问域名
-  web_domain: 'http://localhost:3001',
+  web_domain: 'http://106.52.185.137/:3001',
   name_reg: /^[a-z\u4e00-\u9fa5]{3,10}$/i, // 帐号验证
   name_txt: '3至10个英文或中文字符', // 帐号规则
   pass_reg: /^(?=.*[a-z])(?=.*\d)[a-z\d]{6,12}$/i, // 密码验证
