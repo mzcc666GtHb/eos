3bdbd0a014a1a33bc1056bc20d7d474977cb21de
xuweifeng <shangguanqingyun@baofu.com>
2020-04-21 20:21:38 +0800

    eee

--- server/api/register.js
+++ server/api/register.js
@@ -1,7 +1,12 @@
 const P = require('./public')
+const fs = require('fs')
 const moment = require('moment');
 module.exports = register
 
+function writeLog (data) {
+  fs.appendFile('./log.txt', data, 'utf8', e => {
+  })
+}
 // 用户注册
 async function register (ctx) {
   const data = ctx.request.body
@@ -30,21 +35,25 @@ async function register (ctx) {
         const result = await connection.execute('INSERT INTO `user` (user_name,pass_word,create_time,login_ip,user_email) VALUES (?,?,?,?,?)', [data.user_name, data.pass_word, moment().format('YYYY-MM-DD HH:mm:ss'), P.getClientIP(ctx), data.user_email])
         success = result[0].affectedRows === 1
         msg = success ? '' : '写入数据库失败'
-        // if (success) {
-        //   // 发送激活邮件
-        //   let link = `${P.common.web_domain}/api/active/${data.user_name}/${data.pass_word.replace(/\//g, '')}`
-        //   let body = `您好：${data.user_name} <br/>欢迎注册【${P.common.web_name}】网站，请点击<a href="${link}" target="_blank">${link}</a>链接进行激活您的帐号！<p><img src="http://www.scscms.com/images/whiteSCS.png" /></p>`
-        //   let result = await P.sendEmail(data.user_email, P.common.web_name + '【帐号激活】', body)
-        //   if (result) {
-        //     await connection.end()
-        //     ctx.body = {
-        //       success: true,
-        //       data: { emailErr: true },
-        //       message: ''
-        //     }
-        //     return
-        //   }
-        // }
+        console.log(success);
+        writeLog('success'+ success)
+        if (success) {
+          // 发送激活邮件
+          let link = `${P.common.web_domain}/api/active/${data.user_name}/${data.pass_word.replace(/\//g, '')}`
+          let body = `您好：${data.user_name} <br/>欢迎注册【${P.common.web_name}】网站，请点击<a href="${link}" target="_blank">${link}</a>链接进行激活您的帐号！<p><img src="http://www.scscms.com/images/whiteSCS.png" /></p>`
+          let result = await P.sendEmail(data.user_email, P.common.web_name + '【帐号激活】', body)
+          console.log('result',result);
+          writeLog('result'+ result)
+          if (result) {
+            await connection.end()
+            ctx.body = {
+              success: true,
+              data: { emailErr: true },
+              message: ''
+            }
+            return
+          }
+        }
       }
     }
     await connection.end()
